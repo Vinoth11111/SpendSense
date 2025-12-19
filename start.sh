11385bd0b3aedbe1fc1acaf817631666ce520e9c
VINOTH KUMAR V <vk9360781721@gmail.com>
2025-12-19 21:44:43 +0530

    changed worker for aws

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ streamlit run frontend.py --server.port 8501 --server.address 0.0.0.0 &
 
 #exec gunicorn backend:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
 # in modern gunicron is not necessary, fast apis univcorn itself have workers for handling multiple requests
-exec fastapi run --workers 4 --host 0.0.0.0 --port 8000 backend.py 
+exec fastapi run --workers 1 --host 0.0.0.0 --port 8000 backend.py 
